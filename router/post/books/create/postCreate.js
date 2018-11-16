409381e181e7b1650b382178f99f8d2e1d18b81b
Handle <mail@bysb.net>
2018-11-16 22:16:09 +0800

    未经测试的新建图书信息

---
+++ router/post/books/create/postCreate.js
@@ -0,0 +1,77 @@
+'use strict'
+
+const express = require('express')
+const multer = require('multer')
+const mysqlClient = require('../../../../lib/sql/mysqlClient')
+
+/**
+ * 登出 Post 请求路由
+ */
+const routerPostCreate = express.Router()
+
+/**
+ * 登出
+ * @param {express.Request} req Request对象
+ * @param {express.Response} res Response对象
+ * @param {express.NextFunction} next NextFunction对象
+ */
+const routerFunction = async (req, res, next) => {
+  if (!req.body) return res.sendStatus(400)
+  const {
+    id,
+    type,
+    name,
+    state,
+    author,
+    translator,
+    press,
+    pages,
+    price,
+    publishNumber,
+    publishTime,
+    isbn,
+    brief,
+    outline,
+    isPeriodical,
+    periodicalID,
+    remarks
+  } = req.body
+  const result = await mysqlClient.tables.books.insert([
+    // id,
+    null,
+    type,
+    name,
+    state,
+    author,
+    translator,
+    press,
+    pages,
+    price,
+    publishNumber,
+    publishTime,
+    isbn,
+    brief,
+    outline,
+    isPeriodical,
+    periodicalID,
+    remarks
+  ])
+  if (result.insertId) {
+    res.send({
+      status: 0,
+      info: 'Insert Succ.'
+    })
+  } else {
+    res.send({
+      status: 99,
+      info: 'Unknown Wrons.'
+    })
+  }
+  next()
+}
+
+routerPostCreate.post('/create', multer, routerFunction)
+
+// crypto.createHash('SHA-256').update('9981797954')
+
+module.exports = routerPostCreate
---
+++ router/post/books/postBooksMain.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const express = require('express')
+/**
+ * 用户 Post 总路由
+ */
+const routerBooksMain = express.Router()
+
+// 新增书籍 Post 路由
+const postCreate = require('./login/postCreate')
+// 检查满足用户组 路由
+const checkUserGroup = require('../../all/checkUserGroup')
+
+routerBooksMain.use('/books', checkUserGroup(1), postCreate)
+
+module.exports = routerBooksMain
